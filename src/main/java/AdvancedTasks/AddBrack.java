09ac1831b60bd3fba0e926af8d2e97e1eee91464
Karina <k.tesleva@dunice.net>
2022-05-18 20:07:30 +0300

    Test

--- src/main/java/AdvancedTasks/AddBrack.java
+++ src/main/java/AdvancedTasks/AddBrack.java
@@ -1,45 +1,41 @@
-﻿package AdvancedTasks;
+package AdvancedTasks;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class AddBrack {
     public static void main(String[] args) {
-
         Scanner s = new Scanner(System.in);
+        System.out.println("Впиши любое число или строку:");
+
         String w = s.nextLine();
         String[] str = w.split("");
         s.close();
 
-        int num = str.length / 2;
-
+        float num = Math.round(str.length / 2);
         List list = new LinkedList();
         List list2 = new LinkedList();
 
         for (int i = 0; i < str.length; i++) {
-            if ((str.length % 2 == 0) & i < num) {
+            if (i <= num) {
+                list.add("(");
                 list.add(str[i]);
             }
-            if ((str.length % 2 == 0) & i >= num) {
-                list2.add(str[i]);
-            }
-            if ((str.length % 2 == 1) & i < num + 1) {
-                list.add(str[i]);
-            }
-            if ((str.length % 2 == 1) & i >= num + 1) {
+            if (i > num) {
+                list2.add(")");
                 list2.add(str[i]);
             }
         }
-
-        String word = String.join("(", list);
-        String word2 = String.join(")", list2);
+        String word = String.join("", list);
+        String word2 = String.join("", list2);
 
         if (str.length % 2 == 0) {
-            System.out.println("(" + word + "()" + word2 + ")");
+            System.out.println(word + word2 + ")");
         }
         if (str.length % 2 == 1) {
-            System.out.println("(" + word + ")" + word2 + ")");
+            System.out.println(word + "(" + word2 + ")");
         }
     }
 }
