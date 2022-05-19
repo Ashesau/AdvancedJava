6eafcf71afca61b1528af08a2be46287bf0429cc
Karina <k.tesleva@dunice.net>
2022-05-19 23:46:51 +0300

    Array commit

--- src/main/java/AdvancedTasks/IdenticalPairs.java
+++ src/main/java/AdvancedTasks/IdenticalPairs.java
@@ -23,8 +23,7 @@ public class IdenticalPairs {
         int count = 0;
         for(i = 0; i < nums.length; i++) {
             for(j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j] && i < j );
-                count++;
+                if (nums[i] == nums[j]) count++;
             }
         }
         System.out.println(count);
